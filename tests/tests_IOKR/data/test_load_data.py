edc4cce63caea825e21255d930c865f040f641e4
DanSchl <60350433+DaSch17@users.noreply.github.com>
2022-04-15 11:05:00 +0200

    Update load_data.py

--- tests/tests_IOKR/data/test_load_data.py
+++ tests/tests_IOKR/data/test_load_data.py
@@ -19,8 +19,7 @@ class TestLoadBibtex():
         print(load)
         assert load[0] is not None, "Expected variable: 'X'"
         assert load[1] is not None, "Expected variable: 'Y'"
-        assert load[2] != "", "Expected variable: 'X_txt'"
-        assert load[3] != "", "Expected variable: 'Y_txt'"
+
 
     def test_returned_variables_good_type(self):
         """Test checking if returned variables from load_bibtex are the expected type
@@ -32,15 +31,11 @@ class TestLoadBibtex():
         load = load_bibtex()
         actual_x = type(load[0])
         actual_y = type(load[1])
-        actual_x_txt = type(load[2])
-        actual_y_txt = type(load[3])
         expected1 = "np.array"
-        expected2 = 'list'
         print(actual_x, actual_y, actual_x_txt, actual_y_txt)
         assert isinstance(load[0], np.ndarray), f"'X' should be {expected1}, but is {actual_x} "
         assert isinstance(load[1], np.ndarray), f"'Y' should be {expected1}, but is {actual_y} "
-        assert isinstance(load[2], list), f"'X_txt' should be {expected2}, but is {actual_x_txt} "
-        assert isinstance(load[3], list), f"'Y_txt' should be {expected2}, but is {actual_y_txt} "
+
 
     def test_returned_variables_good_shape(self):
         """Test checking if returned variables from load_bibtex are the expected shape
@@ -52,17 +47,11 @@ class TestLoadBibtex():
         load = load_bibtex()
         actual_x_shape = load[0].shape
         actual_y_shape = load[1].shape
-        actual_x_txt_len = len(load[2])
-        actual_y_txt_len = len(load[3])
         expected_x_shape = (7395, 1836)
         expected_y_shape = (7395, 149)
-        expected_x_txt_len = 1836
-        expected_y_txt_len = 149
         print(actual_x_shape, actual_y_shape, actual_x_txt_len, actual_y_txt_len)
-        assert actual_x_shape, f"'X' should be {expected_x_shape}, but is {actual_x_shape} "
-        assert actual_y_shape, f"'Y' should be {expected_y_shape}, but is {actual_y_shape} "
-        assert actual_x_txt_len, f"'X_txt' should be {expected_x_txt_len}, but is {actual_x_txt_len} "
-        assert expected_y_txt_len, f"'Y_txt' should be {expected_y_txt_len}, but is {actual_y_txt_len} "
+        assert actual_x_shape == expected_x_shape, f"'X' should be {expected_x_shape}, but is {actual_x_shape} "
+        assert actual_y_shape == expected_y_shape, f"'Y' should be {expected_y_shape}, but is {actual_y_shape} "
 
 
 #    def test_check_X_y(self):
